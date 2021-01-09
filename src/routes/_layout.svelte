e4125fc8d4896b5ab57b8e223ab47c0dc37f03ee
Shijie Zhou <cs5129606@gmail.com>
2021-01-09 21:47:38 +0800

    Global css fixed

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -65,20 +65,49 @@
 </script>
 
 <style>
+    .app-container {
+        flex: 1 1 auto;
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+        max-width: 100%;
+        position: relative;
+    }
+
     main {
         position: relative;
         max-width: 56em;
-        background-color: white;
         padding: 2em;
         margin: 0 auto;
         box-sizing: border-box;
+        flex: 1 1 auto;
+        height: 100%;
+        width: 100%;
+    }
+
+    .footer-container {
+        font-size: 10px;
+
+        align-items: center;
+        display: flex;
+        flex: 0 1 auto !important;
+        flex-wrap: wrap;
+        padding: 6px 16px;
+        position: relative;
+        transition-duration: .2s;
+        transition-property: background-color, left, right;
+        transition-timing-function: cubic-bezier(.4, 0, .2, 1);
     }
 </style>
 
-<header>
-  <Nav {navigations} {segment}/>
-</header>
-<main>
-  <slot></slot>
-</main>
-<footer>this is footer</footer>
+<div class="app-container">
+  <header>
+    <Nav {navigations} {segment}/>
+  </header>
+  <main>
+    <slot></slot>
+  </main>
+  <footer class="footer-container">Created by&nbsp;Shijie Zhou 2015 - &copy; { new Date().getFullYear() }</footer>
+</div>
