2f6a0e31aa86dae84c5b33f49c7d0c4b2f76353b
Shijie Zhou <cs5129606@gmail.com>
2021-01-17 20:22:52 +0800

    Adding exclusive page.

--- src/routes/exclusive.svelte
+++ src/routes/exclusive.svelte
@@ -1,3 +1,10 @@
-<h1>Exclusive</h1>
+<style>
+    .exclusive-container {
 
-<p>Exclusive page</p>
+    }
+</style>
+
+<div>
+  <iframe allow="autoplay; encrypted-media" allowfullscreen frameborder="0" height="500ox"
+          src="https://www.youtube.com/embed/-lpiBILzK1c" width="100%"></iframe>
+</div>
