0ce2f0fd3563cedb485fff4615f4a6d826c6cf5f
Shijie Zhou <cs5129606@gmail.com>
2021-01-24 20:11:17 +0800

    Adding Google analytics.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,10 @@
 <script>
     import Nav from '../components/Nav.svelte';
     import { navigations } from '../config/index';
+    import GoogleAnalytics from "sapper-google-analytics/GoogleAnalytics.svelte"
+    import { stores } from "@sapper/app"
+
+    let ga_measurment_id = "UA-149847604-1";
 
     export let segment;
 </script>
@@ -49,6 +53,7 @@
 
 <div class="app-container">
   <header>
+    <GoogleAnalytics id={ga_measurment_id} {stores}/>
     <Nav {navigations} {segment}/>
   </header>
   <main>
