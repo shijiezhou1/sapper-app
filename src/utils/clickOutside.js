5165aaaef1e02154228c547593aeb419f5b9b6b4
Shijie Zhou <shijie.zhou@r4pid.com>
2021-06-10 23:45:55 +0800

    Updating clickoutside

--- src/utils/clickOutside.js
+++ src/utils/clickOutside.js
@@ -1,19 +1,19 @@
 /** Dispatch event on click outside of node */
-export function clickOutside( node ) {
+export function clickOutside(node) {
 
     const handleClick = event => {
-        if ( node && !node.contains( event.target ) && !event.defaultPrevented ) {
+        if (node && !node.contains(event.target) && !event.defaultPrevented) {
             node.dispatchEvent(
-                new CustomEvent( 'clickOutside', node )
+                new CustomEvent('clickOutside', node)
             )
         }
     }
 
-    document.addEventListener( 'click', handleClick, true );
+    document.addEventListener('click', handleClick, true);
 
     return {
         destroy() {
-            document.removeEventListener( 'click', handleClick, true );
+            document.removeEventListener('click', handleClick, true);
         }
     }
 }
