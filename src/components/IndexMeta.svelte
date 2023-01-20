f5aa1f161d78f9dddd8e21ec24c97a6dcea04e6e
shijie <cs5129606@gmail.com>
2023-01-20 11:52:40 +0800

    Moving google analytics to header

--- src/components/IndexMeta.svelte
+++ src/components/IndexMeta.svelte
@@ -15,6 +15,3 @@
   content="https://shijiezhou1.github.io/portfolio-v1/images/newlogo.png"
 />
 <meta property="og:image:alt" content="A image of SHIJIE ZHOU" />
-<!-- google adsenses -->
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1322891097402572"
-     crossorigin="anonymous"></script>
