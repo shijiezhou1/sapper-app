9df6444a6c4ff6ffba8065de716652c377518a7d
Shijie Zhou <28111591+shijiezhou1@users.noreply.github.com>
2026-09-10 22:46:29 +0800

    optimize

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,19 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		include: [
+			'svelte-fa',
+			'@fortawesome/free-brands-svg-icons',
+			'@fortawesome/free-regular-svg-icons',
+			'@fortawesome/free-solid-svg-icons'
+		]
+	},
+	server: {
+		warmup: {
+			client: ['./src/**/*.{js,svelte}'],
+			ssr: ['./src/hooks.server.js', './src/routes/**/+layout*']
+		}
+	}
 });
